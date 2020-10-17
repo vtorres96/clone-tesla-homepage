0ea00d0552fb907d946d8bd78764c8a08ae33fdd
vtorres96 <victorcdc96@gmail.com>
2020-10-17 15:28:47 -0300

    added components: Page component

---
+++ src/components/Page/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import { ModelsWrapper, ModelSection } from '../Model'
+import DefaultOverlayContent from '../DefaultOverlayContent'
+import UniqueOverlay from '../UniqueOverlay'
+
+import { Container, Spacer } from './styles'
+
+const Page: React.FC = () => {
+  return (
+    <Container>
+      <ModelsWrapper>
+        <div>
+          {[
+            'Model S',
+            'Model 3',
+            'Model X',
+            'Model Y'
+          ].map(modelName => (
+            <ModelSection
+              key={modelName}
+              className="colored"
+              modelName={modelName}
+              overlayNode={
+                <DefaultOverlayContent
+                  label={modelName}
+                  description="Order Online for Delivery"
+                />
+              }
+            />
+          ))}
+        </div>
+
+        <Spacer />
+
+        <UniqueOverlay />
+      </ModelsWrapper>
+    </Container>
+  )
+}
+
+export default Page
