37ed0ee5ad7f39c54097795525a9179d4697bf5a
vtorres96 <victorcdc96@gmail.com>
2020-10-17 15:29:00 -0300

    added component styles: Page styles

---
+++ src/components/Page/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  .colored:nth-child(1) {
+    background: #f1ffe7;
+  }
+  .colored:nth-child(2) {
+    background: #dfffd9;
+  }
+  .colored:nth-child(3) {
+    background: #cdfeca;
+  }
+  .colored:nth-child(4) {
+    background: #bbfebb;
+  }
+`
+
+export const Spacer = styled.div`
+  height: 15vh;
+  background: #77e68c;
+`
